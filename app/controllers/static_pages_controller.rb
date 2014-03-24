0e5836ca11cf6f6f08d91180d8221cb24aa19229
CJ Avilla <cjavilla@gmail.com>
2014-03-24 08:55:47 -0700

    try again with first city

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def root
-    render :json => Geocoder.search(request.remote_ip)
+    render :json => Geocoder.search(request.remote_ip).first.city
   end
 end
